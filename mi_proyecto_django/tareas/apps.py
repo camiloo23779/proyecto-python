e003a55ede5bf40ae56eeddb6241e6c577a8740f
camiloo23779 <camilolozada159@gmail.com>
2025-11-29 06:50:28 -0500

    Delete mi_proyecto_django directory
    Correccion

--- mi_proyecto_django/tareas/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-class TareasConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tareas'
-
-    def ready(self):
-        # Importar signals para que se registren (no hacer import en módulo global)
-        import tareas.signals  # noqa
